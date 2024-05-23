ed9cd246f06163758bede7c4dda7eadf7c472112
Muamer999 <muamerzulum@gmail.com>
2024-05-23 14:26:40 +0200

    add checkstyle and surefire plugin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.printf("Hello World!");
-        System.out.printf("Hello Technikum!");
+        System.out.println("Hello World!");
+        System.out.println("Hello Technikum!");
     }
 }
