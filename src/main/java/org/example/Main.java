6610338d2b70289c445bb6588e5c83a4e08f4737
Muamer999 <muamerzulum@gmail.com>
2024-05-23 15:55:20 +0200

    add greeting translator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+
+        GreetingTranslator greetingTranslator = new GreetingTranslator();
+
+        String greeting = greetingTranslator.getGreeting(Locale.FRANCE);
+        greeting += " Technikum!!!";
+
+        System.out.println(greeting);
+
         System.out.println("Hello World!");
         System.out.println("Hello Technikum!");
     }
